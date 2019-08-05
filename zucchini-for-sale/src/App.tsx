acdd17dbfba0aa9a44ad84184d1547a249659a13
Shaun Luttin <shaunluttin@gmail.com>
2019-08-04 21:48:39 -0700

    Render a simple map

--- zucchini-for-sale/src/App.tsx
+++ zucchini-for-sale/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import mapboxgl from "mapbox-gl";
+
+const mapboxAccessToken =
+  "pk.eyJ1IjoibXlsb2NhbGZhcm0iLCJhIjoiY2p5eHE3eHBzMWUxazNjb3ptcHk2OWx3YiJ9.OEbf6j5a-YzNara0zUHHMA";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const container = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    mapboxgl.accessToken = mapboxAccessToken;
+    if (!container.current) return;
+
+    new mapboxgl.Map({
+      container: container.current,
+      style: "mapbox://styles/mapbox/streets-v11"
+    });
+  }, [container]);
+
+  return <div className="map-container" ref={container} />;
+};
 
 export default App;
