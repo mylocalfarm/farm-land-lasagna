7f7ab42f1865ee868b79268bb0242ea88c62a832
Shaun Luttin <shaunluttin@gmail.com>
2019-08-17 12:23:31 -0700

    Render simple map at /mapbox

--- farm-land-lasanga/ClientAppTypeScript/src/App.tsx
+++ farm-land-lasanga/ClientAppTypeScript/src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import "./App.css";
 import MapWithLeaflet from "./components/MapWithLeaflet";
 import MapWithMapbox from "./components/MapWithMapbox";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 /**
  * TODO Add dynamic React routing for the different maps.
  */
 const App: React.FC = () => {
   return (
     <div className="App">
-      <MapWithMapbox />
-      <MapWithLeaflet />
+      <Router>
+        <Route path="/mapbox" component={MapWithMapbox} />
+        <Route path="/leaflet" component={MapWithLeaflet} />
+      </Router>
     </div>
   );
 };
--- farm-land-lasanga/ClientAppTypeScript/src/components/MapWithMapbox.tsx
+++ farm-land-lasanga/ClientAppTypeScript/src/components/MapWithMapbox.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+const mapboxAccessToken =
+  "pk.eyJ1IjoibXlsb2NhbGZhcm0iLCJhIjoiY2p5eHE3eHBzMWUxazNjb3ptcHk2OWx3YiJ9.OEbf6j5a-YzNara0zUHHMA";
 
 const MapWithMapbox: React.FC = () => {
-  return <div>Hello from Mapbox</div>;
+  const container = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    mapboxgl.accessToken = mapboxAccessToken;
+    if (!container.current) return;
+
+    new mapboxgl.Map({
+      container: container.current,
+      style: "mapbox://styles/mapbox/streets-v11"
+    });
+  }, [container]);
+
+  return <div className="map-container" ref={container} />;
 };
 
 export default MapWithMapbox;
