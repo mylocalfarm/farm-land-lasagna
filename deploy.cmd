5279900e5c6102a9ab8dbba4f02063f7eaa62e37
Shaun Luttin <shaunluttin@gmail.com>
2019-08-18 15:21:44 -0700

    Deploy directly to the deployment target

--- deploy.cmd
+++ deploy.cmd
@@ -71,11 +71,7 @@ call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\farm-land-lasanga\FarmLandL
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish
-call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\farm-land-lasanga\FarmLandLasanga.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 3. KuduSync
-call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\farm-land-lasanga\FarmLandLasanga.csproj" --output "%DEPLOYMENT_TARGET%" --configuration Release
 IF !ERRORLEVEL! NEQ 0 goto error
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
